56f5728d7fef1c3696609a9b402cd52fcaa4be33
KeepGen <keeper.gen@gmail.com>
2021-04-25 21:24:15 +0300

    Added Benefits section with styles

--- front-page.php
+++ front-page.php
@@ -36,10 +36,70 @@
             <?php the_field('video_url') ?>
          </div><!-- /.video-section-video -->
 
-         <!-- <img src="<?php echo get_template_directory_uri() . '/assets/images/leaves.png'?>" alt="Leaves decoration" class="video-section-leaves-img"> -->
-
       </div><!-- /.video-section-wrapper -->
    </div><!-- /.container -->
 </div><!-- /.video-section -->
+
+<div class="benefits-section">
+   <div class="container">
+      <div class="benefits-section-wrapper">
+         <?php 
+         $image = get_field('benefits_image');
+         if( !empty( $image ) ): ?>
+            <img src="<?php echo esc_url($image['url']); ?>" alt="<?php echo esc_attr($image['alt']); ?>" class="benefits-section-img" />
+         <?php endif; ?>
+         
+         <div class="benefits-section-text">
+            <h2 class="benefits-section-title"><?php the_field('benefits_title') ?></h2>
+            <p class="benefits-section-description"><?php the_field('benefits_text') ?></p>
+            <div class="benefits-features">
+               <div class="benefits-features-item">
+                  <svg width="25" height="20" fill="#21a2ca" class="icon arrow-icon">
+                     <use xlink:href="<?php echo get_template_directory_uri() ?>/assets/images/sprite.svg#arrow"></use>
+                  </svg>
+                  <span class="benefits-features-item-text"><?php the_field('benefit_#1') ?></span>
+               </div><!-- /.benefits-features-item -->
+
+               <div class="benefits-features-item">
+                  <svg width="25" height="20" fill="#21a2ca" class="icon arrow-icon">
+                     <use xlink:href="<?php echo get_template_directory_uri() ?>/assets/images/sprite.svg#arrow"></use>
+                  </svg>
+                  <span class="benefits-features-item-text"><?php the_field('benefit_#2') ?></span>
+               </div><!-- /.benefits-features-item -->
+
+               <div class="benefits-features-item">
+                  <svg width="25" height="20" fill="#21a2ca" class="icon arrow-icon">
+                     <use xlink:href="<?php echo get_template_directory_uri() ?>/assets/images/sprite.svg#arrow"></use>
+                  </svg>
+                  <span class="benefits-features-item-text"><?php the_field('benefit_#3') ?></span>
+               </div><!-- /.benefits-features-item -->
+
+               <div class="benefits-features-item">
+                  <svg width="25" height="20" fill="#21a2ca" class="icon arrow-icon">
+                     <use xlink:href="<?php echo get_template_directory_uri() ?>/assets/images/sprite.svg#arrow"></use>
+                  </svg>
+                  <span class="benefits-features-item-text"><?php the_field('benefit_#4') ?></span>
+               </div><!-- /.benefits-features-item -->
+
+               <div class="benefits-features-item">
+                  <svg width="25" height="20" fill="#21a2ca" class="icon arrow-icon">
+                     <use xlink:href="<?php echo get_template_directory_uri() ?>/assets/images/sprite.svg#arrow"></use>
+                  </svg>
+                  <span class="benefits-features-item-text"><?php the_field('benefit_#5') ?></span>
+               </div><!-- /.benefits-features-item -->
+
+               <div class="benefits-features-item">
+                  <svg width="25" height="20" fill="#21a2ca" class="icon arrow-icon">
+                     <use xlink:href="<?php echo get_template_directory_uri() ?>/assets/images/sprite.svg#arrow"></use>
+                  </svg>
+                  <span class="benefits-features-item-text"><?php the_field('benefit_#6') ?></span>
+               </div><!-- /.benefits-features-item -->
+            </div><!-- /.benefits-features -->
+         </div><!-- /.benefits-section-text -->
+
+      </div><!-- /.benefits-section-wrapper -->
+   </div><!-- /.container -->
+</div><!-- /.benefits-section -->
+
 
 <?php get_footer(); ?>
